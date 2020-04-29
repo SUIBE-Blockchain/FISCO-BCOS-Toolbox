7ab512fca38854beecfe51a2ec53211d3a529077
99kies <1290017556@qq.com>
2020-04-30 00:04:40 +0800

    update

---
+++ fisco_bcos_toolbox/forms.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""Public forms."""
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField
+from wtforms.validators import DataRequired
+
+from fisco_bcos_toolbox.models import User
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(LoginForm, self).validate()
+        if not initial_validation:
+            return False
+
+        self.user = User.query.filter_by(username=self.username.data).first()
+        if not self.user:
+            self.username.errors.append("Unknown username")
+            return False
+
+        if not self.user.check_password(self.password.data):
+            self.password.errors.append("Invalid password")
+            return False
+
+        if not self.user.active:
+            self.username.errors.append("User not activated")
+            return False
+        return True
+
+class RegisterForm(FlaskForm):
+    """Register form."""
+
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=3, max=25)]
+    )
+    email = StringField(
+        "Email", validators=[DataRequired(), Email(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        "Verify password",
+        [DataRequired(), EqualTo("password", message="Passwords must match")],
+    )
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(RegisterForm, self).validate()
+        if not initial_validation:
+            return False
+        user = User.query.filter_by(username=self.username.data).first()
+        if user:
+            self.username.errors.append("Username already registered")
+            return False
+        user = User.query.filter_by(email=self.email.data).first()
+        if user:
+            self.email.errors.append("Email already registered")
+            return False
+        return True
