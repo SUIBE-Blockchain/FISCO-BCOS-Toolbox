dadfba6821dfb515a5fb9a2c214d94eb69488f9c
99kies <1290017556@qq.com>
2020-05-02 09:27:51 +0800

    update login and register

--- fisco_bcos_toolbox/blueprints/public/public.py
+++ fisco_bcos_toolbox/blueprints/public/public.py
@@ -9,12 +9,12 @@ from flask import (
     request,
     url_for,
 )
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 
 from fisco_bcos_toolbox.extensions import login_manager
 from fisco_bcos_toolbox.forms import RegisterForm, LoginForm
 from fisco_bcos_toolbox.models import User
-from fisco_bcos_toolbox.utils import flash_errors
+from fisco_bcos_toolbox.utils import flash_errors, redirect_back
 
 public_bp = Blueprint("public", __name__, static_folder="../static")
 
@@ -44,24 +44,35 @@ def logout():
     flash("You are logged out.", "info")
     return redirect(url_for("public.home"))
 
+
+
 @public_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    """Login user."""
+    # login in func
     if current_user.is_authenticated:
         return redirect(url_for('public.home'))
 
     form = LoginForm()
     if form.validate_on_submit():
-        username = form.username.data
+        username_or_email = form.username.data
         password = form.password.data
-        user = User.query.filter(User.username==username).first()
-        if user:
-            if user.check_password(password):
-                login_user(user)
+        # remember = form.remember.data
+        user = [User.query.filter(User.username==username_or_email).first(), User.query.filter(User.email==username_or_email).first()]
+        if user[0]:
+            if user[0].check_password(password):
+                # login_user(user[0], remember)
+                login_user(user[0])
                 flash('Welcome back.', 'info')
                 return redirect_back()
             else:
-                flash('账号或者密码错误，请重新输入！', 'warning') 
+                flash('账号或者密码错误，请重新输入！', 'warning')
+        elif user[1]:
+            if user[1].check_password(password):
+                login_user(user[1])
+                flash('Welcome back.', 'info')
+                return redirect_back()
+            else:
+                flash('账号或者密码错误，请重新输入！', 'warning')    
         else:
             flash('No account.', 'warning')
     return render_template('public/login.html', form=form)
@@ -75,17 +86,33 @@ def register():
         
     form = RegisterForm(request.form)
     if form.validate_on_submit():
-        User.create(
-            username=form.username.data,
-            email=form.email.data,
-            password=form.password.data,
-            active=False,
-        )
-        flash("感谢注册，请联系管理员激活账号！", "success")
-        return redirect(url_for("public.home"))
-    else:
-        flash_errors(form)
-    return render_template("public/register.html", form=form)
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        password1 = form.confirm.data
+        if not all([username, email, password, password1]):
+            flash('请把信息填写完整')
+        elif password != password1:
+            flash('两次密码不一致，请重新输入！')
+        elif User.query.filter(User.username==username).first():
+            flash('这个用户名已经被注册过了！')
+        elif User.query.filter(User.email==email).first():
+            flash('这个邮箱已经被注册过了！')
+        else:
+            new_user = User(username=username, email=email, id=None)
+            new_user.set_password(password)
+            db.session.add(new_user)
+            # try:
+            #     db.session.commit()
+                
+            #     return redirect(url_for('public.home'))
+            # except:
+            #     flash("注册失败，请重试！")
+            #     db.session.rollback()
+            db.session.commit()
+            return redirect(url_for('public.login'))
+    return render_template('public/register.html', form=form)
+
 
 
 @public_bp.route("/about/")
