d90a806857ff2354f7bb9886c602a6f79f341a7b
leeduckgo <albertschr@163.com>
2020-05-06 12:16:44 +0800

    feat: add sdk_config.html

--- fisco_bcos_toolbox/user/views.py
+++ fisco_bcos_toolbox/user/views.py
@@ -2,6 +2,7 @@
 """User views."""
 from flask import Blueprint, render_template
 from flask_login import login_required
+from fb_python_sdk.client_config import client_config
 
 blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
 
@@ -16,4 +17,5 @@ def members():
 @login_required
 def sdk_config():
     """List members."""
-    return render_template("users/sdk_config.html")
+    # TODO handle url and posts
+    return render_template("users/sdk_config.html", client_config=client_config)
