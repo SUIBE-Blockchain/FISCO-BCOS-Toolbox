e46bfb4f3020756508ea0df6f0ba6e67479aede4
99kies <1290017556@qq.com>
2020-05-07 19:08:55 +0800

    add admin blueprint

---
+++ fisco_bcos_toolbox/blueprints/admin/admin.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from flask import (
+    Blueprint,
+    current_app,
+    flash,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+from flask_login import login_required, login_user, logout_user, current_user
+
+from fisco_bcos_toolbox.extensions import login_manager
+from fisco_bcos_toolbox.forms import RegisterForm, LoginForm
+from fisco_bcos_toolbox.models import User
+from fisco_bcos_toolbox.utils import flash_errors, redirect_back
+from fisco_bcos_toolbox.extensions import db
+
+admin_bp = Blueprint("admin", __name__, static_folder="../static")
+
+@admin_bp.route('/')
+@login_required
+def index():
+    if current_user.is_admin:
+        users = User.query.order_by(User.created_at.desc()).all()
+        print(users)
+        return render_template('admin/admin.html', users=users)
+    else:
+        redirect_back()
+
+@admin_bp.route('/addactive', methods=['GET', 'POST'])
+@login_required
+def addactive():
+    username = request.args.get('username')
+    user = User.query.filter(User.username==username).first()
+    user.active = True
+    db.session.commit()
+    return redirect_back()
+
+@admin_bp.route('/cancleactive', methods=['GET', 'POST'])
+@login_required
+def cancleactive():
+    username = request.args.get('username')
+    user = User.query.filter(User.username==username).first()
+    user.active = False
+    db.session.commit()
+    return redirect_back()
