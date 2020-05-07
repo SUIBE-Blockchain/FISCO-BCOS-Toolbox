e2134594334ac4b496708aaa2090d75a9a1967e4
leeduckgo <albertschr@163.com>
2020-05-07 12:47:19 +0800

    feat: add mock data preliminary

--- fisco_bcos_toolbox/public/views.py
+++ fisco_bcos_toolbox/public/views.py
@@ -40,7 +40,9 @@ def home():
             return redirect(redirect_url)
         else:
             flash_errors(form)
-    return render_template("public/home.html", form=form)
+    # TODO
+    mock_data = ["byte32", "int", "bytes"]
+    return render_template("public/home.html", form=form, mock_data = mock_data)
 
 
 @blueprint.route("/logout/")
