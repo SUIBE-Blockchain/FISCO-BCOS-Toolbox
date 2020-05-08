19184c14e189c54fcd039375cd9474a7e4d11d6a
leeduckgo <albertschr@163.com>
2020-05-09 06:35:57 +0800

    feat: fix a bug in login

--- fisco_bcos_toolbox/forms.py
+++ fisco_bcos_toolbox/forms.py
@@ -12,6 +12,7 @@ class LoginForm(FlaskForm):
 
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField('Login')
 
     def __init__(self, *args, **kwargs):
         """Create instance."""
