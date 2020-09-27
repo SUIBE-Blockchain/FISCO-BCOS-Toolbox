536582db68c886bc4218eddbe8cde2e94ac02367
利牧羊 <1332045221@qq.com>
2020-09-27 20:42:12 +0800

    Update ethereum.py

--- fisco_bcos_toolbox/blockchain/ethereum.py
+++ fisco_bcos_toolbox/blockchain/ethereum.py
@@ -17,7 +17,7 @@ class Ethereum:
         "payload":
         {"addr": account.address, 
         "priv": account.privateKey.hex(),
-        "pubv": str(account._key_obj.public_key)
+        "pubv": account.address
          }}
     
     @staticmethod
