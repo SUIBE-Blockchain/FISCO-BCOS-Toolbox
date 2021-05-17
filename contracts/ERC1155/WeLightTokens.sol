7b1fde42d8805039c08c0242c2ef0fd06858f630
leeduckgo <albertschr@163.com>
2021-05-17 16:15:44 +0800

    feat/update erc1155

---
+++ contracts/ERC1155/WeLightTokens.sol
@@ -0,0 +1,22 @@
+import "./ERC1155.sol";
+
+contract WeLightTokens is ERC1155 {
+    address public governance;
+    uint256 public airlineCount;
+
+    modifier onlyGovernance() {
+        require(msg.sender == governance, "only governance can call this");
+
+        _;
+    }
+
+    constructor(string memory uri,address governance_) public ERC1155(uri) {
+        governance = governance_;
+        airlineCount = 0;
+    }
+
+    function addNewAirline(uint256 initialSupply, uint256 tokenID) external onlyGovernance {
+        airlineCount ++;
+        _mint(msg.sender, tokenID, initialSupply, "");
+    }
+}
